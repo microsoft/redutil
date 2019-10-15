d6489d9e41013659287805fa91553ddd6ea2345d
Mark Dietzer <git@doridian.net>
2019-10-15 14:19:28 -0700

    feat: Allow usage of redisc cluster client (#33)

--- conn/conn.go
+++ conn/conn.go
@@ -1,9 +1,12 @@
 package conn
 
 import (
+	"crypto/tls"
+	"net"
 	"time"
 
-	"github.com/garyburd/redigo/redis"
+	"github.com/gomodule/redigo/redis"
+	"github.com/mna/redisc"
 )
 
 // Used to denote the parameters of the redis connection.
@@ -19,30 +22,68 @@ type ConnectionParam struct {
 	Timeout time.Duration
 	// Whether or not to secure the connection with TLS
 	UseTLS bool
+	// Whether to use clustering (redisc)
+	UseCluster bool
+}
+
+// RedUtilPool is used as a generic for redis.Pool and redisc.Cluster
+type RedUtilPool interface {
+	Get() redis.Conn
 }
 
 // NewWithActiveLimit makes and returns a pointer to a new Connector instance. It sets some
 // defaults on the ConnectionParam object, such as the policy, which defaults to
 // a LogReconnectPolicy with a base of 10ms. A call to this function does not
 // produce a connection.
-func NewWithActiveLimit(param ConnectionParam, maxIdle int, maxActive int) (*redis.Pool, ReconnectPolicy) {
+func NewWithActiveLimit(param ConnectionParam, maxIdle int, maxActive int) (RedUtilPool, ReconnectPolicy) {
 	if param.Policy == nil {
 		param.Policy = &LogReconnectPolicy{Base: 10, Factor: time.Millisecond}
 	}
 
-	return &redis.Pool{Dial: connect(param), MaxIdle: maxIdle, MaxActive: maxActive}, param.Policy
+	options := make([]redis.DialOption, 0)
+	if param.UseTLS {
+		options = append(options, redis.DialUseTLS(param.UseTLS))
+	}
+
+	if param.Password != "" {
+		options = append(options, redis.DialPassword(param.Password))
+	}
+
+	if param.Timeout > 0 {
+		options = append(options, []redis.DialOption{
+			redis.DialConnectTimeout(param.Timeout),
+			redis.DialReadTimeout(param.Timeout),
+			redis.DialWriteTimeout(param.Timeout),
+		}...)
+	}
+
+	if param.UseCluster {
+		host, _, err := net.SplitHostPort(param.Address)
+		if err != nil {
+			panic(err)
+		}
+
+		options = append(options, redis.DialTLSConfig(&tls.Config{
+			InsecureSkipVerify: false,
+			ServerName:         host,
+		}))
+
+		return &redisc.Cluster{
+			StartupNodes: []string{param.Address},
+			DialOptions:  options,
+			CreatePool:   connectPool(maxIdle, maxActive),
+		}, param.Policy
+	}
+
+	return &redis.Pool{Dial: connect(param.Address, options...), MaxIdle: maxIdle, MaxActive: maxActive}, param.Policy
 }
 
 // New makes and returns a pointer to a new Connector instance. It sets some
 // defaults on the ConnectionParam object, such as the policy, which defaults to
 // a LogReconnectPolicy with a base of 10ms. A call to this function does not
 // produce a connection.
-func New(param ConnectionParam, maxIdle int) (*redis.Pool, ReconnectPolicy) {
-	if param.Policy == nil {
-		param.Policy = &LogReconnectPolicy{Base: 10, Factor: time.Millisecond}
-	}
-
-	return &redis.Pool{Dial: connect(param), MaxIdle: maxIdle, MaxActive: 0}, param.Policy
+func New(param ConnectionParam, maxIdle int) (RedUtilPool, ReconnectPolicy) {
+	return NewWithActiveLimit(param, maxIdle, 0)
 }
 
 // connect is a higher-order function that returns a function that dials,
@@ -58,26 +99,14 @@ func New(param ConnectionParam, maxIdle int) (*redis.Pool, ReconnectPolicy) {
 // If an error is incurred either dialing the TCP connection, or sending the
 // `AUTH` command, then it will be returned immediately, and the client can be
 // considered useless.
-func connect(param ConnectionParam) func() (redis.Conn, error) {
-	return func() (cnx redis.Conn, err error) {
-		options := make([]redis.DialOption, 0)
-		if param.UseTLS {
-			options = append(options, redis.DialUseTLS(param.UseTLS))
-		}
+func connect(address string, options ...redis.DialOption) func() (redis.Conn, error) {
+	return func() (redis.Conn, error) {
+		return redis.Dial("tcp", address, options...)
+	}
+}
 
-		if param.Password != "" {
-			options = append(options, redis.DialPassword(param.Password))
-		}
-
-		if param.Timeout > 0 {
-			options = append(options, []redis.DialOption{
-				redis.DialConnectTimeout(param.Timeout),
-				redis.DialReadTimeout(param.Timeout),
-				redis.DialWriteTimeout(param.Timeout),
-			}...)
-		}
-
-		cnx, err = redis.Dial("tcp", param.Address, options...)
-		return
+func connectPool(maxIdle int, maxActive int) func(string, ...redis.DialOption) (*redis.Pool, error) {
+	return func(address string, options ...redis.DialOption) (*redis.Pool, error) {
+		return &redis.Pool{Dial: connect(address, options...), MaxIdle: maxIdle, MaxActive: maxActive}, nil
 	}
 }
